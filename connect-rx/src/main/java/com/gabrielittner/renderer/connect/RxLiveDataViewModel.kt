7d01df62462312fb1657b21de5d0b694f84db0da
Gabriel Ittner <gabriel.ittner@gmail.com>
2019-10-26 14:11:53 +0200

    fix rx formatting

--- connect-rx/src/main/java/com/gabrielittner/renderer/connect/RxLiveDataViewModel.kt
+++ connect-rx/src/main/java/com/gabrielittner/renderer/connect/RxLiveDataViewModel.kt
@@ -5,23 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 
-abstract class RxLiveDataViewModel<State, Action> :
-    LiveDataViewModel<State, Action> {
+abstract class RxLiveDataViewModel<State, Action> : LiveDataViewModel<State, Action> {
 
-    private val liveData: MutableLiveData<State> =
-        MutableLiveData()
+    private val liveData: MutableLiveData<State> = MutableLiveData()
 
-    private val actionSubject: PublishSubject<Action> =
-        PublishSubject.create()
+    private val actionSubject: PublishSubject<Action> = PublishSubject.create()
 
     protected val actions: Observable<Action> = actionSubject.hide()
 
-    final override fun handleAction(action: Action) {
-        actionSubject.onNext(action)
-    }
-
     protected fun updateState(state: State) {
         liveData.value = state
+    }
+
+    final override fun handleAction(action: Action) {
+        actionSubject.onNext(action)
     }
 
     final override fun observe(): LiveData<State> = liveData
