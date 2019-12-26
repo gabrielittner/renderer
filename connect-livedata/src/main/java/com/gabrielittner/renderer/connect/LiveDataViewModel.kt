974d75caebcc89019139f2d1b61113147cfd3eb3
Gabriel Ittner <gabriel@freeletics.com>
2019-12-26 19:26:24 +0100

    introduce connect overload for Fragment to use viewLifecycleOwner by default

--- connect-livedata/src/main/java/com/gabrielittner/renderer/connect/LiveDataViewModel.kt
+++ connect-livedata/src/main/java/com/gabrielittner/renderer/connect/LiveDataViewModel.kt
@@ -1,5 +1,6 @@
 package com.gabrielittner.renderer.connect
 
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -8,6 +9,13 @@ import com.gabrielittner.renderer.Renderer
 interface LiveDataViewModel<State, Action> {
     fun handleAction(action: Action)
     fun observe(): LiveData<State>
+}
+
+fun <State, Action> Fragment.connect(
+    renderer: Renderer<State, Action>,
+    model: LiveDataViewModel<State, Action>
+) {
+    viewLifecycleOwner.connect(renderer, model)
 }
 
 fun <State, Action> LifecycleOwner.connect(
