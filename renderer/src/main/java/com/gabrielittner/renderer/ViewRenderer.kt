0fa35412b7934ccf8f1e84d84c7e0c90ac611d9c
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-01-29 09:33:24 +0100

    make ViewBindingFactory the only factory, change signature for assissted inject

--- renderer/src/main/java/com/gabrielittner/renderer/ViewRenderer.kt
+++ renderer/src/main/java/com/gabrielittner/renderer/ViewRenderer.kt
@@ -83,72 +83,16 @@ abstract class ViewRenderer<State, Action>(
     protected abstract fun renderToView(state: State)
 
     /**
-     * Simple factory to create a [ViewRenderer] from an already inflated [View].
+     * A factory that inflates the a [ViewBinding] using the given [bindingFactory].
      */
-    interface Factory<State, Action> {
-        /**
-         * Create a [ViewRenderer] for the given [rootView].
-         */
-        fun create(rootView: View): ViewRenderer<State, Action>
-    }
-
-    /**
-     * Factory to create a [ViewRenderer] with a newly inflated [View]. See [LayoutInflaterFactory]
-     * and [ViewBindingFactory] for more concrete implementations.
-     */
-    interface InflaterFactory<State, Action> {
-        /**
-         * Inflates a [View] in the given [parent] and then creates a [ViewRenderer] for it.
-         */
-        fun inflate(parent: ViewGroup): ViewRenderer<State, Action>
-
-        /**
-         * Inflates a [View] in the given [parent] and then creates a [ViewRenderer] for it.
-         */
-        fun inflate(inflater: LayoutInflater, parent: ViewGroup?): ViewRenderer<State, Action>
-    }
-
-    /**
-     * An [InflaterFactory] that inflates the a [View] using the given [layoutId].
-     */
-    abstract class LayoutInflaterFactory<State, Action>(
-        private val layoutId: Int
-    ) : InflaterFactory<State, Action> {
-        /**
-         * Inflates a [View] from [layoutId] in the given [parent] and then creates a [ViewRenderer]
-         * for it.
-         */
-        final override fun inflate(parent: ViewGroup): ViewRenderer<State, Action> {
-            val inflater = LayoutInflater.from(parent.context)
-            return inflate(inflater, parent)
-        }
-
-        /**
-         * Inflates a [View] from [layoutId] in the given [parent] and then creates a [ViewRenderer]
-         * for it.
-         */
-        override fun inflate(inflater: LayoutInflater, parent: ViewGroup?): ViewRenderer<State, Action> {
-            val view = inflater.inflate(layoutId, parent, false)
-            return create(view)
-        }
-
-        /**
-         * Create a [ViewRenderer] for the given [rootView].
-         */
-        protected abstract fun create(rootView: View): ViewRenderer<State, Action>
-    }
-
-    /**
-     * An [InflaterFactory] that inflates the a [ViewBinding] using the given [bindingFactory].
-     */
-    abstract class ViewBindingFactory<Binding : ViewBinding, State, Action>(
+    abstract class Factory<Binding : ViewBinding, R : ViewRenderer<*, *>>(
         private val bindingFactory: (LayoutInflater, ViewGroup?, Boolean) -> Binding
-    ) : InflaterFactory<State, Action> {
+    ) {
         /**
          * Inflates a [ViewBinding] using [bindingFactory] in the given [parent] and then creates
          * a [ViewRenderer] for it.
          */
-        final override fun inflate(parent: ViewGroup): ViewRenderer<State, Action> {
+        fun inflate(parent: ViewGroup): R {
             val inflater = LayoutInflater.from(parent.context)
             return inflate(inflater, parent)
         }
@@ -157,14 +101,14 @@ abstract class ViewRenderer<State, Action>(
          * Inflates a [ViewBinding] using [bindingFactory] in the given [parent] and then creates
          * a [ViewRenderer] for it.
          */
-        final override fun inflate(inflater: LayoutInflater, parent: ViewGroup?): ViewRenderer<State, Action> {
+        fun inflate(inflater: LayoutInflater, parent: ViewGroup?): R {
             val binding = bindingFactory(inflater, parent, false)
             return create(binding)
         }
 
         /**
-         * Create a [ViewRenderer] for the given [binding].
+         * Create a [ViewBindingRenderer] for the given [binding].
          */
-        protected abstract fun create(binding: Binding): ViewRenderer<State, Action>
+        protected abstract fun create(binding: Binding): R
     }
 }
